09b05529fc2f35b8aab087d681430e0c76d71697
Cesar Espinoza Herrera <andresherrera232425@gmail.com>
2025-05-27 13:08:35 -0600

    creacion correcta del archivo
    se mantienen los cambios de lo que se habia hecho hasta el momento solo que estaba en un archivo incorrecto, los cambios son las librerias, y las funciones principales

---
+++ Código/proyecto2.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import random
+
+#ventana principal 
+ventana =Tk()
+ventana.title("Tetris")
+
+#funciones principales
+def nuevo_juego():
+    print ()
+
+def cargar_juego():
+    print()
+
+def guardar_juego():
+    print()
+
+def estadisticas ():
+    print ("a Nombre del jugador")
+    print ("b Puntaje Obtenido")
+    print ("c Mostrar el Ranking o posición con respecto a otros jugadores (Top 10)")
