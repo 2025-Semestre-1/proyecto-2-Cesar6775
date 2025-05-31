d50347d826cd8fd0e30e664f9e8f02f34bb68718
Cesar Espinoza Herrera <andresherrera232425@gmail.com>
2025-05-31 11:36:17 -0600

    creacion de la pieza y movimiento
    se crea la pieza en el canvas, y se puede mover, aun no se implementa la rotacion, ya cae automáticamente la pieza, solo falta que se detenga con las colisiones y las demas cosas

--- Código/proyecto2.py
+++ Código/proyecto2.py
@@ -1,7 +1,8 @@
+#importacion de librerias
 from tkinter import *
 import random
+import time
 
-#
 #creacion de piezas
 pieza_O = [[1, 1],[1, 1]]
 
@@ -19,17 +20,16 @@ pieza_U = [[7, 0, 7],[7, 7, 7]]
 
 pieza_mas = [[0, 8, 0],[8, 8, 8],[0, 8, 0]]
 
-
 todas_las_piezas = [pieza_O, pieza_I, pieza_L, pieza_J, pieza_T, pieza_Z, pieza_U, pieza_mas]
 
-#
+
 #ventana principal 
 ventana =Tk()
 ventana.title("Tetris")
 canvas = Canvas(ventana, width=700, height=800, bg="lightgray")
 canvas.pack()
 
-#gobales 
+#funciones globales 
 TAM_CELDA = 30
 EXTRA = 10 
 ruta = ""
@@ -39,7 +39,6 @@ tablero = []
 
     
 #funciones principales
-
 def cargar_tablero(ruta):
     with open(ruta,"r") as archivo:
         lineas = archivo.readlines()
@@ -108,9 +107,99 @@ def dibujar_tablero(canvas, tablero):
                 color = "Yellow"
             
             canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
-            
-boton_iniciar = Button(ventana, text="Iniciar Juego")
+ 
+ 
+#funcion de creacion de pieza
+ 
+def generar_pieza():
+    global pieza_actual
+    global posicion_pieza
+    pieza_actual = random.choice(todas_las_piezas)  
+    posicion_pieza = [4, 0]  
+
+def dibujar_pieza():
+    global pieza_actual, posicion_pieza
+    if pieza_actual is not None:
+        for i in range(len(pieza_actual)):
+            fila = pieza_actual[i]
+            for j in range(len(fila)):
+                celda = fila[j]
+                if celda > 0:
+                    x1 = (posicion_pieza[0] + j) * TAM_CELDA + EXTRA
+                    y1 = (posicion_pieza[1] + i) * TAM_CELDA + EXTRA
+                    x2 = x1 + TAM_CELDA
+                    y2 = y1 + TAM_CELDA
+                    
+                    if celda == 1:
+                        color = "blue"
+                    elif celda == 2:
+                        color = "Red"
+                    elif celda == 3:
+                        color = "Black"
+                    elif celda == 4:
+                        color = "Green"
+                    elif celda == 5:
+                        color = "Orange"
+                    elif celda == 6:
+                        color = "Purple"
+                    elif celda == 7:
+                        color = "Brown"
+                    else:
+                        color = "Yellow"
+                    
+                    canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
+
+def mover_pieza_hacia_abajo():
+    global posicion_pieza
+    posicion_pieza[1] += 1  # Mueve la pieza hacia abajo
+    canvas.delete("all")  # Limpia el tablero antes de dibujar
+    dibujar_tablero(canvas, tablero)  # Dibuja el tablero
+    dibujar_pieza()  # Dibuja la pieza en la nueva posición
+    ventana.after(500, mover_pieza_hacia_abajo)  # Llama a la función cada 500ms
+
+#mover la pieza
+def mover_izquierda(event=None):
+    global posicion_pieza
+    posicion_pieza[0] -= 1
+    redibujar()
+
+def mover_derecha(event=None):
+    global posicion_pieza
+    posicion_pieza[0] += 1
+    redibujar()
+
+def mover_abajo(event=None):
+    global posicion_pieza
+    posicion_pieza[1] += 1
+    redibujar()
+    
+#se rebibula la posicion de la pieza
+def redibujar():
+    dibujar_tablero(canvas, tablero)
+    dibujar_pieza()
+
+def iniciar_juego():
+    generar_pieza()
+    dibujar_pieza()
+    mover_pieza_hacia_abajo()  
+
+
+    
+    
+
+ 
+##################################################################################
+ 
+ 
+ 
+#boton de iniciar 
+boton_iniciar = Button(ventana, text="Iniciar Juego", command=iniciar_juego)
 boton_iniciar.place(x=500, y=50)
+
+#teclas de movimiento
+ventana.bind("<Left>", mover_izquierda)
+ventana.bind("<Right>", mover_derecha)
+ventana.bind("<Down>", mover_abajo)
     
 nuevo_juego()
 ventana.mainloop()
